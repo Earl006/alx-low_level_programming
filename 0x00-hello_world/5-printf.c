2798fa9d14adf95d23fd4abba2528226271970f6
Earljoe <earljoe06@gmail.com>
2023-08-10 09:26:14 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A function that uses printf to print a line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
