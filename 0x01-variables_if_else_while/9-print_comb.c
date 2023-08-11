083d0f67500704860b612abe8c8e07dc25c62e0f
Earljoe <earljoe06@gmail.com>
2023-08-11 14:10:34 -0700

    patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all combinations of single digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 0 ; i < 10 ; i++)
+{
+for (j = i + 1 ; j < 10 ; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i != 8 || j != 9)
+{
+putchar(',');
+}
+}
+}
+putchar('\n');
+}
