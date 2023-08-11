96eb06d7d8a738855678b5e2925580db7f3ae5b9
Earljoe <earljoe06@gmail.com>
2023-08-11 14:16:48 -0700

    fixed

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,18 +5,12 @@
  */
 int main(void)
 {
-int i, j;
+int i;
 for (i = 0 ; i < 10 ; i++)
 {
-for (j = i + 1 ; j < 10 ; j++)
-{
 putchar(i + '0');
-putchar(j + '0');
-if (i != 8 || j != 9)
-{
 putchar(',');
-}
-}
+putchar(' ');
 }
 putchar('\n');
 return (0);
