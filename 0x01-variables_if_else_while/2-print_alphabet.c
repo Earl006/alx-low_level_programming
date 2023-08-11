f70aa73f36a7a9d569d5912102df6ff858d359d0
Earljoe <earljoe06@gmail.com>
2023-08-11 12:35:25 -0700

    sewer

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a to z in lowercase using putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z' ; i++)
+{
+putchar(i);
+putchar('\n');
+}
+return (0);
+}
