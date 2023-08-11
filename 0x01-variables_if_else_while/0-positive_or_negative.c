479bbb2c7cbfd002556d7a5eb579564cccab6aa9
Earljoe <earljoe06@gmail.com>
2023-08-11 09:12:05 -0700

    positive and negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main _ A program that takes a random number n and uses if else to get value
+ * Returns: 0 (Success)
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if(n>0)
+{
+printf("%d is positive\n",n);
+}
+else if(n==0)
+{
+printf("%d is zero\n");
+}
+else if(n<0)
+{
+printf("%d is negative\n");
+}
+return (0);
+}
