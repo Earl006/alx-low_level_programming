87178181e1d385f4150b3915543afffba932011d
Earljoe <earljoe06@gmail.com>
2023-08-15 11:01:07 -0700

    good things

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,10 +2,11 @@
 /**
  * print_last_digit - A function that prints the last digit of a number
  * @n: Contains the number
+ * @num: Contains the result of n%10
  * Return: num
  */
-int print_last_digit(int n)
+int print_last_digit(int n, int num)
 {
-num = n % 10
-return (n);
+num = n % 10;
+return (num);
 }
