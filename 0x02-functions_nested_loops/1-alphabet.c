aaef1a5af0774e28e9bab4cf19a619cc8ed4e6d1
Earljoe <earljoe06@gmail.com>
2023-08-15 07:47:54 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+void print_alphabet(void)
+{
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
