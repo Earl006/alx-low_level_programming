b19251ab6dc03f775562298ab9885be2abeabbb9
Earljoe <earljoe06@gmail.com>
2023-08-15 14:19:44 -0700

    tunnel

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_to_98 - A function that prints natural numbers from n to 98
+ * @n: Contains the current number
+ * Return: all natural nmbers below 98
+ */
+void print_to_98(int n)
+{
+while (n != 98)
+{
+_putchar(n);
+_putchar(',');
+if (n > 98)
+{
+n--;
+}
+else
+{
+n++
+}
+}
+putchar(' ');
+}
