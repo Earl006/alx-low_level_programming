fb4446cb0406743326458aadbf2e28a68c80f32c
Earljoe <earljoe06@gmail.com>
2023-08-15 14:32:56 -0700

    98 Battery

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - A function that prints natural numbers from n to 98
  * @n: Contains the current number
@@ -6,18 +7,35 @@
  */
 void print_to_98(int n)
 {
-while (n != 98)
+if (n <= 98)
 {
-_putchar(n);
-_putchar(',');
-if (n > 98)
+for (; n <= 98; n++)
 {
-n--;
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+break;
 }
 else
 {
-n++;
+printf("%d ", n);
 }
 }
-_putchar(' ');
+}
+else
+{
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
 }
