56c45605182b0542cbde943e02fc5d40e9b6e185
Earljoe <earljoe06@gmail.com>
2023-08-17 12:56:47 -0700

    finally

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,10 +6,10 @@
 void print_most_numbers(void)
 {
 char c;
-for (c = '0'; c < '10'; c++)
+for (c = '0'; c <= '9'; c++)
 {
-	if (!(c == 2 || c == '4'))
-	_putchar(c);
-	}
-	_putchar('\n');
+if (!(c == 2 || c == '4'))
+_putchar(c);
+}
+_putchar('\n');
 }
