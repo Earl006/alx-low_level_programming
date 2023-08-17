8fed360cc5c5ea6f9ee435bccc09e7e8ab336217
Earljoe <earljoe06@gmail.com>
2023-08-17 13:41:47 -0700

    opop

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,8 +8,8 @@ void print_most_numbers(void)
 char c;
 for (c = '0'; c <= '9'; c++)
 {
-if (!(c == 2 || c == '4'))
-_putchar(c + '0');
+if (!(c == '2' || c == '4'))
+_putchar(c);
 }
 _putchar('\n');
 }
