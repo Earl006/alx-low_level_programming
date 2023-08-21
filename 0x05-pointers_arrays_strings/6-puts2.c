bbcb750e5006de937f6c51e12a84ff3abcb3d2a3
Earljoe <earljoe06@gmail.com>
2023-08-21 12:35:44 -0700

    six

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - A function that prints every char
+ * @str: char to be printed
+ * Return: void
+ */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		_putchar('\n');
+		*str++
+	}
+}
