62340e1ea53b70e774069a198f89e81e1160652c
Earljoe <earljoe06@gmail.com>
2023-08-21 09:52:39 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that takes a pointer to n
+ * @n: Cointains the value to be converted
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This directory contains arrays and pointers and many more 
+Also some more of C fun
