c904dc6dd904267059d8190629848f1c3c6843fe
Earljoe <earljoe06@gmail.com>
2023-09-11 12:28:30 -0700

    abs

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the name of the file
+ *
+ * Return: Always - (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
